975327db3255b918f058a7e93a384fc63257952e
Damosm <35764575+Damosm@users.noreply.github.com>
2020-10-30 13:27:05 +0100

    image2

--- src/components/ImageGridList.js
+++ src/components/ImageGridList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
+import img2 from '../images/5.jpg';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -22,7 +23,7 @@ export default function ImageGridList(props) {
     const classes = useStyles();
 
     return (
-        <div className={classes.root} style={{ backgroundColor: '#EFECCA' }} >
+        <div className={classes.root} style={{ backgroundColor: '#f8f9fa', backgroundImage: `url(${img2})` }} >
             <GridList cellHeight={160} className={classes.gridList} cols={3}>
                 {props.images.map((tile) => (
                     <GridListTile key={tile.id} cols={1}>
