3bf9bb6d622db6d26ac0a7181e16dfed55c64b75
Damosm <35764575+Damosm@users.noreply.github.com>
2020-11-06 11:41:43 +0100

    HashRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import ImageSearchCss from './ImageSearchCss';
 import Clock from './Clock';
@@ -10,16 +10,16 @@ export default class App extends React.Component {
 
     render() {
         return (
-            <BrowserRouter>
+            <HashRouter basename="/">
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    <a className="navbar-brand" href="/images">Home</a>
+                    <a className="navbar-brand" href="/"><Link to="/">Home</Link></a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavDropdown">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <a className="nav-link" href="/images/imagesCss">Search</a>
+                                <a className="nav-link" href="/"><Link to="/imagesCss">Search</Link></a>
                             </li>                            
                         </ul>
                     </div>
@@ -28,10 +28,10 @@ export default class App extends React.Component {
                     </div>
                 </nav>
                 <div className="main-route-place">
-                    <Route exact path="/images" component={Home} />                    
-                    <Route path="/images/imagesCss" component={ImageSearchCss} />
+                    <Route exact path="/" component={Home} />                    
+                    <Route path="/imagesCss" component={ImageSearchCss} />
                 </div>
-            </BrowserRouter >
+            </HashRouter >
         );
     }
 }
